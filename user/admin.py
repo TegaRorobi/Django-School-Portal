eda3443e171af3d621d6db7213c95cb9009976cf
Tega Rorobi <rorobitega@gmail.com>
2023-07-17 09:27:27 +0100

    Registered the user model in the admin.

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+
+UserModel = get_user_model()
+
+@admin.register(UserModel)
+class UserAdmin(admin.ModelAdmin):
+	model = UserModel
+	list_display = ('username', 'is_staff', 'is_student', 'is_educator')
