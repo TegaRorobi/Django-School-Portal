e7d4838803b89083f68b4cf3cfb218083847aad3
Tega Rorobi <rorobitega@gmail.com>
2023-07-17 08:59:44 +0100

    Modified the default user model and pointed to it in the settings.

--- school_system/settings.py
+++ school_system/settings.py
@@ -126,3 +126,10 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.User'
+
+AUTHENTICATION_BACKENDS = [
+    'user.backends.CustomAuthBackend',
+    'django.contrib.auth.backends.ModelBackend'
+]
--- user/models.py
+++ user/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext as _
 from django.db import models
+import random
+from django.core.validators import EmailValidator
+
 
 # Create your models here.
+class User(AbstractUser):
+
+	@staticmethod
+	def generate_passkey(n_length) -> str:
+		perm = (str(random.randint(0, 9)) for _ in range(n_length))
+		return f"IHEMCNPS-{''.join(perm)}"
+
+	class Meta:
+		unique_together = ['email', 'name']
+
+	username = models.CharField(_('Email'), max_length=150, unique=True, validators=[EmailValidator])
+	name = models.CharField(max_length=200, null=True, blank=False)
+	passkey = models.CharField(max_length=13, default=generate_passkey(4), unique=True, editable=False)
+	is_student = models.BooleanField(default=False)
+	is_educator = models.BooleanField(default=False)
+
+	REQUIRED_FIELDS = []
+	USERNAME_FIELD = 'username'
