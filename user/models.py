1d81fb7df888bf16de70dca91c58e389e57ab0bc
Tega Rorobi <rorobitega@gmail.com>
2023-07-22 13:36:41 +0100

    Added phone and address fields and fixed the bug in the save method where a random passkey was generated on every save.

--- user/models.py
+++ user/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext as _
 from django.db import models, IntegrityError
-from django.core.validators import EmailValidator
+from django.core.validators import EmailValidator, RegexValidator
 import random
 
 
@@ -12,16 +12,20 @@ class User(AbstractUser):
 		return const + ''.join(ret)
 
 	def save(self, *args, **kwargs):
-		while True:
-			try:
-				self.passkey = self.generate_passkey("IHEMCNPS-", 4)
-				super().save(*args, **kwargs)
-				break
-			except IntegrityError:
-				pass
+		if not self.passkey:
+			while True:
+				try:
+					self.passkey = self.generate_passkey("IHEMCNPS-", 4)
+					super().save(*args, **kwargs)
+					break
+				except IntegrityError:
+					pass
+		else:
+			super().save(*args, **kwargs)
+
 
 	class Meta:
-		unique_together = ['email', 'passkey']
+		unique_together = ['username', 'passkey']
 
 	ACCOUNT_TYPE_CHOICES = [
 		("admin", "Admin"),
@@ -34,12 +38,21 @@ class User(AbstractUser):
 		('f', 'Female')
 	]
 
-	name = models.CharField(max_length=200, null=True, blank=False)
+	# Works with Nigeria's country code, but can be modified for any country
+	PHONE_NUMBER_VALIDATOR = RegexValidator(
+		regex = "\+(234)[789][01]\d{8}",
+		message = "Invalid formatting. Here's an example:\n\t'+2348011235813' corresponds to '08011235813'"
+	)
+
+	name = models.CharField(_('Full Name'), max_length=200, null=True, blank=False)
 	username = models.CharField(_('Email'), max_length=150, unique=True, validators=[EmailValidator])
 	gender = models.CharField(max_length=1, null=True, choices=GENDER_CHOICES)
 	account_type = models.CharField(max_length=8, choices=ACCOUNT_TYPE_CHOICES, null=True)
 	dob = models.DateField(_('Date of Birth'), null=True)
-	passkey = models.CharField(max_length=13, unique=True, blank=True)
+	address = models.CharField(max_length=200, null=True)
+	phone = models.CharField(_('Phone Number'), max_length=14, null=True, validators=[PHONE_NUMBER_VALIDATOR])
+	passkey = models.CharField(max_length=13, unique=True, blank=True, editable=False)
+
 
 	REQUIRED_FIELDS = []
 	USERNAME_FIELD = 'username'
