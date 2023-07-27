fd23148e0c82224d498867d56a0616be0e9f5a2d
Tega Rorobi <rorobitega@gmail.com>
2023-07-27 15:50:06 +0100

    Created a model for messages but didn't register it in the admin.

--- main/models.py
+++ main/models.py
@@ -119,3 +119,19 @@ class AdminProfile(models.Model):
 
 	def __str__(self):
 		return str(self.user)
+
+
+
+
+class Message(models.Model):
+	sender = models.ForeignKey(UserModel, related_name='sent_messages', on_delete=models.CASCADE)
+	receiver = models.ForeignKey(UserModel, related_name='received_messages', on_delete=models.CASCADE)
+	content = models.TextField()
+
+	timestamp = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		ordering = ['-timestamp']
+
+	def __str__(self):
+		return f"{self.sender} -> {self.receiver}"
