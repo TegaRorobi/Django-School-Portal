9511518f1adf47f0bf7fbf91ec852159a1b487fa
Tega Rorobi <rorobitega@gmail.com>
2023-11-03 15:26:15 +0100

    Nothing really, added spaces

--- api/views.py
+++ api/views.py
@@ -40,6 +40,8 @@ def getRoutes(request):
 	]
 	return Response(routes)
 
+
+
 class UsersViewSet(viewsets.ModelViewSet):
 	serializer_class = UserSerializer
 	queryset = UserModel.objects.all()
@@ -48,25 +50,35 @@ class UsersViewSet(viewsets.ModelViewSet):
 			return [IsSuperUser()]
 		return [IsAdminOrSuperUserOrReadOnly()]
 
+
+
 class StudentProfilesViewSet(viewsets.ModelViewSet):
 	serializer_class = StudentProfileSerializer
 	queryset = StudentProfile.objects.all()
 	permission_classes = [IsAdminOrSuperUserOrOwnerOrReadOnly]
 	
+
+
 class TeacherProfilesViewSet(viewsets.ModelViewSet):
 	serializer_class = TeacherProfileSerializer
 	queryset = TeacherProfile.objects.all()
 	permission_classes = [IsAdminOrSuperUserOrOwnerOrReadOnly]
+
+
 
 class AdminProfilesViewSet(viewsets.ModelViewSet):
 	serializer_class = AdminProfileSerializer
 	queryset = AdminProfile.objects.all()
 	permission_classes = [IsSuperUserOrOwnerOrReadOnly]
 
+
+
 class SubjectsViewSet(viewsets.ModelViewSet):
 	queryset = Subject.objects.all()
 	serializer_class = SubjectSerializer
 	permission_classes = [IsAdminOrSuperUserOrReadOnly]
+
+
 
 class GradesViewSet(viewsets.ModelViewSet):
 	queryset = Grade.objects.all()
@@ -76,10 +88,14 @@ class GradesViewSet(viewsets.ModelViewSet):
 			return [IsSuperUser()]
 		return [IsAdminOrSuperUserOrReadOnly()]
 
+
+
 class AllMessagesViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
 	queryset = Message.objects.all()
 	serializer_class = AllMessagesSerializer
 	permission_classes = [IsSuperUser]
+
+
 
 class SentMessagesViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
 	serializer_class = SentMessageSerializer
@@ -88,6 +104,8 @@ class SentMessagesViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, mixins
 		return serializer.save(sender=self.request.user)
 	def get_queryset(self):
 		return Message.objects.filter(sender=self.request.user).order_by('-timestamp')
+
+
 
 class ReceivedMessagesViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
 	serializer_class = ReceivedMessageSerializer
