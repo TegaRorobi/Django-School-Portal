fd04a621b5a7041641ba05556e915800a376011e
Tega Rorobi <rorobitega@gmail.com>
2023-11-05 21:49:42 +0100

    Configured logging from django.db.backends with a StreamHandler, to expose query activity, and subsequently enhance performance.

--- school_system/settings.py
+++ school_system/settings.py
@@ -170,3 +170,20 @@ if DEBUG:
     INSTALLED_APPS.append("debug_toolbar")
     MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")
     INTERNAL_IPS = ['127.0.0.1']
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        },
+    },
+}
