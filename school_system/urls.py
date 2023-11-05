2de245fd71816c1bf4346d22c4460d4e405e3233
Tega Rorobi <rorobitega@gmail.com>
2023-11-05 21:48:07 +0100

    Added swagger documentation to the root urlconf, and the documentation covers all api views across the application!

--- school_system/urls.py
+++ school_system/urls.py
@@ -1,31 +1,51 @@
 """
 URL configuration for school_system project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+    info = openapi.Info(
+        title="Django School Portal API",
+        default_version='v1',
+        description="Django School Portal API Documentation",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="support@django.site"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes = (AllowAny,)
+)
+
+# api_version = 'v1'
+
 urlpatterns = [
+    # admin
     path(settings.ADMIN_URL, admin.site.urls),
+
+    # urlconfs
     path('', include('main.urls')),
     path('api/', include('api.urls')),
     path('accounts/', include('user.urls')),
+
+    # rest_framework (mainly auth)
     path('api-auth/', include('rest_framework.urls')),
 
+    # swagger/redoc
+    re_path('^api/swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(), name='schema-json'),
+    re_path('^api/swagger/?$', schema_view.with_ui('swagger'), name='schema-swagger'),
+    re_path('^api/redoc/?$', schema_view.with_ui('redoc'), name='schema-redoc'),
+
+    # media/static
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
     *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 ]
