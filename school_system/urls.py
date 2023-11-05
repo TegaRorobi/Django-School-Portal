509e98b22a7a863ad3fb347897b5d022bf33a772
Tega Rorobi <rorobitega@gmail.com>
2023-11-05 19:28:25 +0100

    Updated the root urlconf to complement the settings with the truthy DEBUG condition.

--- school_system/urls.py
+++ school_system/urls.py
@@ -18,16 +18,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-import debug_toolbar
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
-    path('__debug__/', include(debug_toolbar.urls)),
     path('', include('main.urls')),
-    path('accounts/', include('user.urls')),
     path('api/', include('api.urls')),
-    path('api-auth/', include('rest_framework.urls'))
+    path('accounts/', include('user.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+
+    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    import debug_toolbar
+    urlpatterns.append(path('__debug__/', include(debug_toolbar.urls)))
