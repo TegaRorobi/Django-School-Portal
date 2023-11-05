362c7df9949564b507c1e028d3f8402aa99fb82d
Tega Rorobi <rorobitega@gmail.com>
2023-11-05 19:30:46 +0100

    Created a file for storing aliases to dictionaries, matching http methods to their corresponding view method (probably to be used by Class-based API views)

---
+++ api/aliases.py
@@ -0,0 +1,22 @@
+
+# BASE
+LIST = {'get':'list'}
+
+CREATE = {'post':'create'}
+
+RETRIEVE = {'get':'retrieve'}
+
+UPDATE = {'put':'update'}
+
+PARTIAL_UPDATE = {'patch':'partial_update'}
+
+DESTROY = {'delete':'destroy'}
+
+
+# MULTIPLE 
+LIST_CREATE = {**LIST, **CREATE}
+
+RETRIEVE_DESTROY = {**RETRIEVE, **DESTROY}
+
+RETRIEVE_UPDATE_DESTROY = {**RETRIEVE, **UPDATE, **PARTIAL_UPDATE, **DESTROY}
+
