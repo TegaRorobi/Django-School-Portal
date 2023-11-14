249be29cf80aa39aa386e298c8771e508ac443e8
Tega Rorobi <rorobitega@gmail.com>
2023-11-14 14:25:59 +0100

    Created an Abstract BaseModel class to add common fields and methods that can be used by other model classes.

--- base/models.py
+++ base/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def new(cls, *args, **kwargs) -> models.Model:
+        "Create and save a new model instance"
+        ins = cls.objects.create(*args, **kwargs)
+        return ins
+
+    def update(self, **kwargs) -> None:
+        for k, v in kwargs.items():
+            if hasattr(self, k):
+                setattr(self, k, v)
+        self.save()
